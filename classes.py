a2867ec79d040ebec1d3e49c43c2e1a181b4b0cd
Admin <lucastimothycook@gmail.com>
2018-02-09 22:05:47 -0600

    Ghost Movement Adjustments

--- classes.py
+++ classes.py
@@ -3,7 +3,6 @@ from pygame.locals import *
 from random import randint
 
 class Maze():
-
     def __init__(self):
 	self.BLACK = (0,0,0)
 	self.GOLD = (246,253,49)
@@ -11,7 +10,7 @@ class Maze():
 	self.RED = (255,0,0)
 	self.BLUE = (20,27,229)
 	self.WHITE = (255,255,255)	
-	self.GREEN = (0,255,0)	
+	self.GREEN = (0,255,0)
 	self.width  = 20
 	self.height = 20
 	self.margin = 1	
@@ -21,77 +20,139 @@ class Maze():
 	self.walls = []
 	self.countfinal = 0
 	self.make()
-	self.size = [500, 700]
+	self.size = [400, 600]
 	self.screen = pygame.display.set_mode(self.size)
 
-    def make(self):	
-	for i in range(2,4):
-	    for j in range(5,14):
-		self.walls.append([i,j])
-	for i in range(3,5):
-	    for j in range(7,20):
-		self.walls.append([j,i])
-	for i in range(10,12):
-	    for j in range(10,15):
-		self.walls.append([j,i])
-	for i in range(17,19):
-	    for j in range(0,21):
-		self.walls.append([j,i])
-	for i in range(26,28):
-	    for j in range(0,13):
-		self.walls.append([i,j])
-	for i in range(4,6):
-	    for j in range(31,35):
-		self.walls.append([j,i])
-	for i in range(38,40):
-	    for j in range(2,21):
-		self.walls.append([i,j])
-	for i in range(29,31):
-	    for j in range(18,30):
-		self.walls.append([i,j])
-	for i in range(5,7):
-	    for j in range(24,30):
-		self.walls.append([i,j])
-	for i in range(23,25):
-	    for j in range(10,25):
-		self.walls.append([j,i])
-	
-	for row in range(30):
+    def make(self):
+        
+	for row in range(22):
             self.grid.append([])
-    	    for column in range(40):
+            for column in range(19):
                 self.grid[row].append(0)
-        for wall in self.walls:
-    	    self.grid[wall[1]][wall[0]]=1 	
-	return self
 
+        for i in range(0,4):
+            for j in range(8,14):
+                self.grid[j][i] = 1
+
+        for i in range(15,19):
+            for j in range(8,14):
+                self.grid[j][i] = 1
+
+	#top wall
+        for i in range(0,19):
+            for j in range(0,1):
+                self.walls.append([i,j])
+    
+        #right wall
+        for i in range(18,19):
+            for j in range(0,7):
+                self.walls.append([i,j])
+        for i in range(15,19):
+            for j in range(7,8):
+                self.walls.append([i,j])
+        for i in range(15,16):
+            for j in range(7,9):
+                self.walls.append([i,j])
+        for i in range(15,19):
+            for j in range(9,10):
+                self.walls.append([i,j])
+        for i in range(18,19):
+            for j in range(13,22):
+                self.walls.append([i,j])
+        for i in range(15,19):
+            for j in range(13,14):
+                self.walls.append([i,j])
+        for i in range(15,16):
+            for j in range(11,13):
+                self.walls.append([i,j])
+        for i in range(15,19):
+            for j in range(11,12):
+                self.walls.append([i,j])
+
+        #bottom wall
+        for i in range(0,18):
+            for j in range(21,22):
+                self.walls.append([i,j])
+
+        #left wall
+        for i in range(0,1):
+            for j in range(0,7):
+                self.walls.append([i,j])
+        for i in range(0,4):
+            for j in range(7,8):
+                self.walls.append([i,j])
+        for i in range(3,4):
+            for j in range(7,10):
+                self.walls.append([i,j])
+        for i in range(0,4):
+            for j in range(9,10):
+                self.walls.append([i,j])
+        for i in range(0,4):
+            for j in range(13,14):
+                self.walls.append([i,j])
+        for i in range(3,4):
+            for j in range(11,13):
+                self.walls.append([i,j])
+        for i in range(0,4):
+            for j in range(11,12):
+                self.walls.append([i,j])
+        for i in range(0,1):
+            for j in range(13,22):
+                self.walls.append([i,j])
+	
+	#middle wall
+	for i in range(7,8):
+            for j in range(9,12):
+                self.walls.append([i,j])
+        for i in range(11,12):
+            for j in range(9,12):
+                self.walls.append([i,j])
+	for i in range(7,9):
+            for j in range(9,10):
+                self.walls.append([i,j])
+        for i in range(10,11):
+            for j in range(9,10):
+                self.walls.append([i,j])
+	for i in range(7,11):
+            for j in range(11,12):
+                self.walls.append([i,j])
+
+	for wall in self.walls:
+    	    self.grid[wall[1]][wall[0]] = 1
+
+        return self
+            
     def reset(self):
-	self.grid = []
-	self.walls = []
-	self.countfinal = 0
-	self.make()	
-	return self
+        self.grid = []
+        self.walls = []
+        self.countfinal = 0
+        self.make()
+        return self
 
     def scoredisp(self):
-        scoretext=self.scorefont.render("Score: "+(str)(self.score), 1,self.RED)
-        self.screen.blit(scoretext, (30, 650))
+        scoretext=self.scorefont.render("Score: "+(str)(self.score), 1,self.WHITE)
+        self.screen.blit(scoretext, (30, 560))
 
     def leveldisp(self):
         leveltext=self.scorefont.render("Level: "+(str)(self.level), 1,self.RED)
-        self.screen.blit(leveltext, (600, 650))
+        self.screen.blit(leveltext, (30, 530))
 
     def dispmaze(self):
-	for row in range(30):
-            for column in range(40):
+        olor = self.GREY
+        for row in range(22):
+            for column in range(19):
                 color = self.GREY
 		pygame.draw.rect(self.screen,color,[(self.margin+self.width)*column+self.margin,(self.margin+self.height)*row+self.margin,self.width,self.height])
-                color = self.GOLD
+                color = self.WHITE
                 if self.grid[row][column] == 1:
-		    self.countfinal=self.countfinal+1
-	        else:
-		    pygame.draw.rect(self.screen,color,[(self.margin+self.width)*column+self.margin+7,(self.margin+self.height)*row+self.margin+7,self.width-14,self.height-14])
-
+                    self.countfinal=self.countfinal+1
+                else:
+                    pygame.draw.circle(self.screen,color,[(self.margin+self.width)*column+self.margin+self.width/2,(self.margin+self.height)*row+self.margin+self.width/2], 2)
+		leveltext=self.scorefont.render(str(row), 1,self.BLACK)
+        	self.screen.blit(leveltext, ((self.margin+self.width)*column+self.margin, (self.margin+self.height)*row+self.margin))
+    
     def drawwalls(self):
-	for wall in self.walls:
+        for wall in self.walls:
             pygame.draw.rect(self.screen,self.BLUE,[(self.margin+self.width)*wall[0]+self.margin,(self.margin+self.height)*wall[1]+self.margin,self.width,self.height])
 
 class Person(Maze):
@@ -99,7 +160,9 @@ class Person(Maze):
 	def __init__(self,x,y):
 	    self.x = x	
 	    self.y = y
-	
+	    self.totalMoves = 0
+	    self.stalls = 0
+
 	def checkWall(self,x,y,W):
 	    if [x,y] in W:
 		return True
@@ -107,19 +170,18 @@ class Person(Maze):
 		return False
 
 	def moveleft(self,W):
-	    if self.x > 0:
-		if self.checkWall(self.x-1,self.y,W):                
-                    self.x=self.x
-	        else:
-                    self.x=(self.x)-1
+            if self.x <= 0: self.x = 19
+            
+            if self.checkWall(self.x-1,self.y,W): 
+		self.x=self.x
+	    else: self.x=(self.x)-1
 	    return self		
 
 	def moveright(self,W):
-	    if self.x < 39:
-                if self.checkWall(self.x+1,self.y,W):
-                    self.x=self.x
-	        else:
-                    self.x=(self.x)+1
+            if self.x >= 18: self.x = -1
+    
+            if self.checkWall(self.x+1,self.y,W): self.x=self.x
+            else: self.x=(self.x)+1
 	    return self
 
 	def moveup(self,W):
@@ -128,10 +190,10 @@ class Person(Maze):
                     self.y=self.y
 	        else:
                     self.y=(self.y)-1
-	    return self
+	        return self
 
 	def movedown(self,W):
-            if self.y < 29:
+            if self.y < 21:
                 if self.checkWall(self.x,self.y+1,W):
                     self.y=self.y
 	        else:
@@ -141,8 +203,10 @@ class Person(Maze):
 class Pacman(Person):
 
 	def __init__(self):
-	    x = randint(0,6)
-	    y = randint(0,4)
+	    x = 9
+	    y = 20
+	    totalMoves = 0
+	    stalls = 0
 	    Person.__init__(self,x,y)
 	
 	def resetpacman(self):
@@ -157,28 +221,52 @@ class Pacman(Person):
 		return False
 
 	def pacleft(self,Wa):
+	    x = self.x
 	    Person.moveleft(self,Wa.walls)
+	    self.totalMoves = self.totalMoves + 1
+	    print("Total moves: " + str(self.totalMoves))
+	    if x == self.x:
+		self.stalls = self.stalls + 1
+		print("Stall: " + str(self.stalls))
 	    if self.collectCoin(Wa):	    
 		Wa.grid[self.y][self.x]=1
 		Wa.score += 1
 	    return self
 
 	def pacright(self,Wa):
+	    x = self.x
 	    Person.moveright(self,Wa.walls)
+	    self.totalMoves = self.totalMoves + 1
+	    print("Total moves: " + str(self.totalMoves))
+	    if x == self.x:
+		self.stalls = self.stalls + 1
+		print("Stall: " + str(self.stalls))
 	    if self.collectCoin(Wa):	    
 		Wa.grid[self.y][self.x]=1
 		Wa.score += 1
 	    return self
 
 	def pacup(self,Wa):
-	    Person.moveup(self,Wa.walls)
+	    x = self.x
+            Person.moveup(self,Wa.walls)
+            self.totalMoves = self.totalMoves + 1
+            print("Total moves: " + str(self.totalMoves))
+            if x == self.x:
+                self.stalls = self.stalls + 1
+                print("Stall: " + str(self.stalls))
 	    if self.collectCoin(Wa):	    
 		Wa.grid[self.y][self.x]=1
 		Wa.score += 1
 	    return self
 
 	def pacdown(self,Wa):
-	    Person.movedown(self,Wa.walls)
+	    x = self.x
+            Person.movedown(self,Wa.walls)
+            self.totalMoves = self.totalMoves + 1
+            print("Total moves: " + str(self.totalMoves))
+            if x == self.x:
+                self.stalls = self.stalls + 1
+                print("Stall: " + str(self.stalls))
 	    if self.collectCoin(Wa):	    
 		Wa.grid[self.y][self.x]=1
 		Wa.score += 1
@@ -206,14 +294,14 @@ class Pacman(Person):
 
 class Ghost(Person):
 
-	def __init__(self):
-	    x = randint(20,27)
-	    y = randint(15,22)
+	def __init__(self,newx,newy):
+	    x = newx
+	    y = newy
 	    Person.__init__(self,x,y)
 	
 	def resetghost(self):
-	    x = randint(20,27)
-	    y = randint(15,22)
+	    x = 9
+	    y = 10
 	    Person.__init__(self,x,y)
 	    return self
 
@@ -234,15 +322,38 @@ class Ghost(Person):
 	    return self
 
 	def pos(self,G):
-	    pygame.draw.rect(G.screen,G.RED,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
+	    myimage = pygame.image.load('ghostieR.jpg')
+	    G.screen.blit(myimage,((G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin))
+	    #pygame.draw.rect(G.screen,G.RED,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
  
 	def ghostPosition(self,G):
-	    move = randint(0,3)
-            if move == 0:
-	        self.ghleft(G)
-            elif move == 1:
-	        self.ghright(G)
-            elif move == 2:
-	        self.ghup(G)
-            elif move == 3:
-	        self.ghdown(G)
+	    while 1 == 1:
+		move = randint(0,3)
+            	if move == 0:
+			if self.checkWall(self.x-1,self.y,G.walls):
+				print("WALL! " + str(self.x) + str(self.y))
+				continue
+			else:
+	        		self.ghleft(G)
+				break
+            	elif move == 1:
+			if self.checkWall(self.x+1,self.y,G.walls):
+                                print("WALL! " + str(self.x) + str(self.y))
+                                continue
+                        else:
+                                self.ghright(G)
+                                break
+            	elif move == 2:
+			if self.checkWall(self.x,self.y+1,G.walls):
+                                print("WALL! " + str(self.x) + str(self.y))
+                                continue
+                        else:
+                                self.ghup(G)
+                                break
+            	elif move == 3:
+			if self.checkWall(self.x,self.y-1,G.walls):
+                                print("WALL! " + str(self.x) + str(self.y))
+                                continue
+                        else:
+                                self.ghdown(G)
+                                break
--- pacman.py
+++ pacman.py
@@ -2,13 +2,16 @@ import pygame
 from pygame.locals import * 
 from random import randint
 from classes import *
+from time import sleep
 
 if __name__ == "__main__" or __name__ == "pacman" :
 
     GAME = Maze()
     HERO = Pacman()
-    VILLIAN = Ghost()
-    VILLIAN2 = Ghost()
+    VILLIAN = Ghost(9,4)
+    VILLIAN2 = Ghost(4,10)
+    VILLIAN3 = Ghost(14,10)
+    VILLIAN4 = Ghost(9,16)
     pygame.init()
     GAME.scorefont = pygame.font.Font(None,30)
     done = False
@@ -21,10 +24,14 @@ if __name__ == "__main__" or __name__ == "pacman" :
     	    if event.type == KEYDOWN:
 		if event.key == K_q:
 	            done = True
+
 	HERO.pacPosition(GAME)        
 	GAME.screen.fill(GAME.BLACK)
+	sleep(.1)
 	VILLIAN.ghostPosition(GAME)
 	VILLIAN2.ghostPosition(GAME)        
+	VILLIAN3.ghostPosition(GAME)
+	VILLIAN4.ghostPosition(GAME)
 	move = randint(0,3)
         GAME.countfinal=0
         GAME.dispmaze()
@@ -32,17 +39,24 @@ if __name__ == "__main__" or __name__ == "pacman" :
         HERO.pos(GAME)
         VILLIAN.pos(GAME)
         VILLIAN2.pos(GAME) 
-        if HERO.checkGhost(VILLIAN) or HERO.checkGhost(VILLIAN2):
-	    done = True
-	    print "Final Score = "+(str)(GAME.score)
-        elif GAME.countfinal == 1200 :
+        VILLIAN3.pos(GAME)
+        VILLIAN4.pos(GAME)
+
+        if HERO.checkGhost(VILLIAN) or HERO.checkGhost(VILLIAN2) or HERO.checkGhost(VILLIAN3) or HERO.checkGhost(VILLIAN4):
+            done = True 
+   	    print "Final Score = "+(str)(GAME.score)
+        elif GAME.countfinal == 1200:
 	    GAME.reset()
-	    HERO.resetpacman()
-	    VILLIAN.resetghost()
-	    VILLIAN2.resetghost()
-	    GAME.level += 1	
+            HERO.resetpacman()
+            VILLIAN.resetghost()
+            VILLIAN2.resetghost()
+            VILLIAN3.resetghost()
+            VILLIAN4.resetghost()
+            GAME.level += 1
+	
 	GAME.scoredisp()
 	GAME.leveldisp()        
 	clock.tick(10)
         pygame.display.flip()
+    
     pygame.quit()
